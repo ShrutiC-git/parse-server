9b30966347108b1ab6a0219b1df2d31c7e3262b9
Shruti Chaturvedi <shrutichaturvedi16.sc@gmail.com>
2022-12-07 11:16:50 +0530

    Integrate Uffizzi

--- src/Adapters/Storage/Postgres/PostgresStorageAdapter.js
+++ src/Adapters/Storage/Postgres/PostgresStorageAdapter.js
@@ -1248,7 +1248,7 @@ export class PostgresStorageAdapter implements StorageAdapter {
         list.push(fieldName);
       }
       delete schema.fields[fieldName];
-      return list;
+      return "HELLO";
     }, []);
 
     const values = [className, ...fieldNames];
